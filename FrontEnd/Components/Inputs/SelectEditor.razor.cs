05084e7118f6984a06c52c6a88568fadb2410136
Sascha Davidson <saschadavidson1@gmail.com>
2026-05-19 11:59:40 +0200

    update

--- FrontEnd/Components/Inputs/SelectEditor.razor.cs
+++ FrontEnd/Components/Inputs/SelectEditor.razor.cs
@@ -2,10 +2,8 @@ using Microsoft.AspNetCore.Components;
 
 namespace Playground.FrontEnd.Components.Inputs;
 
-public partial class SelectEditor<TValue>
+public partial class SelectEditor
 {
-    
-
     protected async Task HandleInput(ChangeEventArgs e)
     {
         Value = e.Value?.ToString();
